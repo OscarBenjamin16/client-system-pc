310df7fd9c0053645dad68028e4354309ad8fcee
Carlos Daniel Contreras <tiburonchan3@gmail.com>
2021-06-30 23:38:44 -0600

    all is ok

--- services/catalog.service.ts
+++ services/catalog.service.ts
@@ -1,32 +1,32 @@
 export async function getPaginatedProducts(page: number, search: string,order:number) {
-    const response = await fetch(`/api/producto/products-paginate?pagina=${page}&producto=${search}&order=${order}`)
+    const response = await fetch(`https://systempcs.herokuapp.com/api/producto/products-paginate?pagina=${page}&producto=${search}&order=${order}`)
     return response.json();
 }
 export async function getBestRating(page:number,limit?:number){
-    const response = await fetch(`/api/producto/more-ratings?pagina=${page}&limit=${limit}`)
+    const response = await fetch(`https://systempcs.herokuapp.com/api/producto/more-ratings?pagina=${page}&limit=${limit}`)
     return response.json();
 }
 export async function getMostSellers(page:number){
-    const response = await fetch(`/api/producto/bestSellers?pagina=${page}`)
+    const response = await fetch(`https://systempcs.herokuapp.com/api/producto/bestSellers?pagina=${page}`)
     return response.json();
 }
 export function showImage(name: string | undefined) {
-    return `/api/producto/image?image=${name}`
+    return `https://systempcs.herokuapp.com/api/producto/image?image=${name}`
 }
 
 export async function getProductbyMark(marca: number) {
-    const response = await fetch(`/api/producto/product-marca?marca=${marca}`)
+    const response = await fetch(`https://systempcs.herokuapp.com/api/producto/product-marca?marca=${marca}`)
     return response.json()
 }
 export async function getProductbyCategory(category: number) {
-    const response = await fetch(`/api/producto/product-categoria?categoria=${category}`)
+    const response = await fetch(`https://systempcs.herokuapp.com/api/producto/product-categoria?categoria=${category}`)
     return response.json()
 }
 export async function getProductById(id: string | string[] | number | undefined) {
-    const response = await fetch(`/api/producto/${id}`)
+    const response = await fetch(`https://systempcs.herokuapp.com/api/producto/${id}`)
     return response.json()
 }
 export async function getNewProducts(){
-    const response = await fetch('/api/producto/news')
+    const response = await fetch('https://systempcs.herokuapp.com/api/producto/news')
     return response.json()
 }
