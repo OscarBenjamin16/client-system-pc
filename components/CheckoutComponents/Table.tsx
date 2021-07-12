76b02a39f91f411de9e95a6a046699e47d8a4fbe
Carlos Daniel Contreras <tiburonchan3@gmail.com>
2021-07-12 17:57:59 -0600

    all changes is ok and ready for deploy

--- components/CheckoutComponents/Table.tsx
+++ components/CheckoutComponents/Table.tsx
@@ -37,7 +37,7 @@ const Table = ({ items, setReload, reload, cupon }: Props) => {
     <>
       {items?.length && items.length > 0 ? (
         <>
-          <div className="-mx-6 w-full sm:-mx-8 sm:px-8 overflow-x-auto mt-10">
+          <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto mt-10">
             <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
               <table className="min-w-full leading-normal">
                 <thead>
