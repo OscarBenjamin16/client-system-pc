761816f94e61bebc8ae328fc61cea70031b0530d
OscarBenjamin16 <39130492+OscarBenjamin16@users.noreply.github.com>
2021-09-24 14:56:07 -0600

    Update Layout.tsx
    LA ESPERANZA SE LA COME

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -17,7 +17,7 @@ const Layout = ({ children,reload,setReload }: Props) => {
         <div className="w-screen h-full mt-10 sm:mt-20">{children}</div>
       </header>
       <footer className="py-1 z-50 sm:py-3 font-small sm:text-md fixed w-full xs:mb-0 bottom-0 bg-gray-800 text-white text-center">
-        © Copyright - SystemPC 2021
+        © Copyright - M&E Soporte Tecnico 2021
       </footer>
     </div>
   );
