84b677a65b5369b0a2eb85253e8823500d54c5a0
OscarBenjamin16 <39130492+OscarBenjamin16@users.noreply.github.com>
2021-09-24 14:31:26 -0600

    Update constants.ts
    COCAINA

--- utils/constants.ts
+++ utils/constants.ts
@@ -2,7 +2,6 @@ export const TOKEN = "token";
 
 //export const SERVER_API = "http://localhost:5000/api"
 
-export const SERVER_API = "https://systempcs.herokuapp.com/api";
+export const SERVER_API = "https://mye-soporte-server.herokuapp.com/api"
 
-
-export const SOCKET_URL = "https://systempcs.herokuapp.com/"
+export const SOCKET_URL = "https://mye-soporte-server.herokuapp.com/"
